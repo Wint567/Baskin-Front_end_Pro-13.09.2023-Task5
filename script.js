7271d385e06f83323f740893fef89dde5db210b6
Wint567 <134635875+Wint567@users.noreply.github.com>
2023-09-13 21:08:20 +0300

    task5

---
+++ script.js
@@ -0,0 +1,52 @@
+var birthYear = prompt("Введіть свій рік народження:");
+
+if (birthYear !== null && birthYear !== "") {
+    var city = prompt("Введіть місто, в якому ви живете:");
+
+    if (city !== null && city !== "") {
+        var sport = prompt("Введіть свій улюблений вид спорту:");
+
+        if (sport !== null && sport !== "") {
+            var message = "Ваш вік: " + (new Date().getFullYear() - birthYear) + " років\n";
+
+            if (city === "Київ" || city === "Вашингтон" || city === "Лондон") {
+                message += "Ти живеш у столиці " + getCountry(city) + ".";
+            } else {
+                message += "Ти живеш у місті " + city + ".";
+            }
+
+            // Частина №2 - Ваші види спорту та чемпіони
+            var sportsChampions = {
+                "футбол": "Ліонель Мессі",
+                "баскетбол": "Леброн Джеймс",
+                "теніс": "Рафаель Надаль"
+            };
+
+            if (sportsChampions[sport.toLowerCase()]) {
+                message += "\nКруто! Хочеш стати " + sportsChampions[sport.toLowerCase()] + "?";
+            }
+
+            alert(message);
+        } else {
+            alert("Шкода, що ви не захотіли ввести свій улюблений вид спорту.");
+        }
+    } else {
+        alert("Шкода, що ви не захотіли ввести своє місто.");
+    }
+} else {
+    alert("Шкода, що ви не захотіли ввести свій рік народження.");
+}
+
+function getCountry(capital) {
+    switch (capital) {
+        case "Київ":
+            return "України";
+        case "Вашингтон":
+            return "Сполучених Штатів Америки";
+        case "Лондон":
+            return "Великої Британії";
+        default:
+            return "цієї країни";
+    }
+}
+
